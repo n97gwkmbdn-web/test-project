a5aeaeac5305c9ef85c3c6ed1104107c2b49d339
n97gwkmbdn-web <seishin-karate@i.softbank.jp>
2025-11-19 16:44:55 +0900

    +　更新

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
 // main.js
-const today = new Date();
-console.log(`今日は ${today.toLocaleDateString()} です`);
+const number1 = document.querySelector('h1');
+const plusButton1 = document.querySelector('button');
+
+let count = 0;
+
+plusButton1.addEventListener('click',function() {
+    count ++;
+    number1.innerHTML = count;
+});
+
+const number = document.querySelector('h1');
+const plusButton = document.querySelector('button2');
+
+plusButton.addEventListener('click',function() {
+    count --;
+    number.innerHTML = count;
+});
